1bacf2e5899a13dbef09e3d9655ee7a275ecb35d
onidemon37 <41202410+onidemon37@users.noreply.github.com>
2022-06-14 20:53:27 +0100

    go comments

--- src/hello_world/main.go
+++ src/hello_world/main.go
@@ -1,7 +1,13 @@
 package main
 
 import "fmt"
+/*
+This is one of three ways of using comments
+*/
+
+// This is also a comment
+// and this as well
 
 func main() {
-    fmt.Println("Hello Brave New World of Go")
+    fmt.Println("Hello Brave New World of Go") // This is a trailing comments
 }

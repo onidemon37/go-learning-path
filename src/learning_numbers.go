b46e020c489458e96e3e0e2abe9e248f139eca93
onidemon37 <41202410+onidemon37@users.noreply.github.com>
2022-06-15 10:15:46 +0100

    go numbers

---
+++ src/learning_numbers.go
@@ -0,0 +1,16 @@
+ackage main
+
+import (
+  "fmt"
+  "math"
+)
+
+func main() {
+	// integers
+	fmt.Println("Addition:", 1+3)
+	fmt.Println("Subtraccion:", 1-3)
+	fmt.Println("Multiplication:", 2*3)
+	// float64
+	fmt.Println("Division float:", 20.0/3)
+	fmt.Println("Exponents:", math.Pow(20.0, 3))
+}

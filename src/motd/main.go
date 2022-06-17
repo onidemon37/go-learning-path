af1c0e6c95cdd3af126b804fa228cd4c543582b8
onidemon37 <41202410+onidemon37@users.noreply.github.com>
2022-06-17 18:08:38 +0100

    Interacting with files

--- src/motd/main.go
+++ src/motd/main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"motd/message"
 	"os"
 	"strings"
 )
 
 func main() {
+	f, err := os.OpenFile("/etc/motd", os.O_WRONLY, 644)
+
+	if err != nil {
+		fmt.Println("Error: Unable to open /etc/motd")
+		os.Exit(1)
+	}
+	defer f.Close()
+
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Your Greeting: ")
 	phrase, _ := reader.ReadString('\n')
@@ -18,6 +27,11 @@ func main() {
 	name, _ := reader.ReadString('\n')
 	name = strings.TrimSpace(name)
 
-	message := message.Greeting(name, phrase)
-	fmt.Println(message)
+	m := message.Greeting(name, phrase)
+	err = ioutil.WriteFile("/etc/motd", []byte(m), 0644)
+
+	if err != nil {
+		fmt.Println("Unable to write /etc/motd")
+		os.Exit(1)
+	}
 }

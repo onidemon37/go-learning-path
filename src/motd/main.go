5a3290bd425100aa95d5c2e423ab53f7632df881
onidemon37 <41202410+onidemon37@users.noreply.github.com>
2022-06-18 14:08:48 +0100

    creating a cli

--- src/motd/main.go
+++ src/motd/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
-	"io/ioutil"
 	"motd/message"
 	"os"
 	"strings"
@@ -11,45 +11,71 @@ import (
 
 func main() {
 	// define flags
+	var name string
+	var greeting string
+	var prompt bool
+	var preview bool
 
 	// Parse flags
+	flag.StringVar(&name, "name", "", "name to use within the message")
+	flag.StringVar(&greeting, "greeting", "", "greeting to use before the message")
+	flag.BoolVar(&prompt, "prompt", false, "use prompt to input a name and greeting")
+	flag.BoolVar(&preview, "preview", false, "Use preview to output message without writting to /etc/motd")
+
+	flag.Parse()
 
 	// Show usage if flags are invalid
-
-	// Conditionally read from stdin
+	if prompt == false && (name == "" || greeting == "") {
+		flag.Usage()
+		os.Exit(1)
+	}
 
 	// Optionally print flags and exit based on DEBUG environment variables
+	if os.Getenv("DEBUG") != "" {
+		fmt.Println("Name:", name)
+		fmt.Println("Greeting:", greeting)
+		fmt.Println("Prompt:", prompt)
+		fmt.Println("Preview:", preview)
 
+		os.Exit(1)
+	}
+	// Conditionally read from stdin
+
+	if prompt {
+		name, greeting = renderPrompt()
+	}
 	// Generate message
+	message := message.Greeting(name, greeting)
 
 	// Either preview message or write to file
+	if preview {
+		// Write content
+		f, err := os.OpenFile("./etc_motd", os.O_WRONLY, 644)
 
-	// Write content
+		if err != nil {
+			fmt.Println("Error: Unable to open /etc/motd")
+			os.Exit(1)
+		}
+		defer f.Close()
+
+		// _, err = ioutil.WriteFile("./etc_motd", []byte(m), 0644)
+		_, err = f.Write([]byte(message))
+
+		if err != nil {
+			fmt.Println("Unable to write /etc/motd")
+			os.Exit(1)
+		}
+	}
 }
 
-func main() {
-	f, err := os.OpenFile("/etc/motd", os.O_WRONLY, 644)
-
-	if err != nil {
-		fmt.Println("Error: Unable to open /etc/motd")
-		os.Exit(1)
-	}
-	defer f.Close()
-
+func renderPrompt() (name, greeting string) {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Your Greeting: ")
-	phrase, _ := reader.ReadString('\n')
-	phrase = strings.TrimSpace(phrase)
+	greeting, _ = reader.ReadString('\n')
+	greeting = strings.TrimSpace(greeting)
 
 	fmt.Print("Your Name: ")
-	name, _ := reader.ReadString('\n')
+	name, _ = reader.ReadString('\n')
 	name = strings.TrimSpace(name)
-
-	m := message.Greeting(name, phrase)
-	err = ioutil.WriteFile("/etc/motd", []byte(m), 0644)
-
-	if err != nil {
-		fmt.Println("Unable to write /etc/motd")
-		os.Exit(1)
-	}
+	return
 }
